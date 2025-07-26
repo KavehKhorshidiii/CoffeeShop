870dda921730638a31f4fe1b323bc39a4dc4cb7a
Kaveh <kavehkhorshidiii@gmail.com>
2025-07-26 23:59:30 +0330

    Development of the shopping cart

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,7 @@
-
-//? Kaveh Khorshidi
-
 import Header from "@/components/header/header";
 
 
-
 export default function Home() {
-
 
   return (
       <div className=" bg-[url(/bg-img/bgi.webp)] bg-contain bg-no-repeat bg-gray-100 dark:bg-zinc-800">
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -20,9 +20,37 @@ export default function header() {
         <span className=' text-amber-50'><TfiLayoutLineSolid className=' rotate-90 text-amber-50'></TfiLayoutLineSolid></span>
 
         <div className='flex ml-2'>
-          <div className='flex items-center'><FaBasketShopping className=' w-7 h-7'/></div>
+          <div className='flex items-center py-2 group relative'>
+            <FaBasketShopping className=' w-7 h-7'/>
+
+              <div className=' absolute text-base space-y-4 w-sm transition-all child:transition-colors tracking-normal shadow-2xl flex flex-col items-end bg-white dark:bg-zinc-700 dark:text-white rounded-2xl border-t-[3px]  group-hover:opacity-100 group-hover:visible border-orange-300 text-zinc-700 p-2 top-[100%] left-0 '>
+                {/* opacity-0 invisible */}
+                <div className=' w-full border-2'>
+                  مشاهده
+                </div>
+
+                <div className=' border-2 w-full h-30'>
+                  محصول
+                </div>
+                
+                <div className='flex w-full justify-between'>
+                  <button className=' cursor-pointer rounded-2xl border-2 py-3.5 px-6'>ثبت سفارش</button>
+                  <div className=' border-2 flex flex-col'>
+                    <span>مبلغ قابل پرداخت</span>
+                    <span>
+                      500,000
+                      <span>تومان</span>
+                    </span>
+                  </div>
+                </div>
+
+              </div>
+
+          </div>
         </div>
+
       </div>
+
       <div className=' flex gap-3.5  items-center'>
 
         <a href="">تماس با ما</a>
