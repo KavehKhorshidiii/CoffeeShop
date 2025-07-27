2d13f47955750d0460654a2552d8659858a478c3
Kaveh <kavehkhorshidiii@gmail.com>
2025-07-27 18:29:10 +0330

    shopping cart section  is completed

--- src/components/header/shopCartHeader/shopCartHeader.js
+++ src/components/header/shopCartHeader/shopCartHeader.js
@@ -1,80 +1,65 @@
 import React from "react";
+import { MdOutlineArrowBackIos } from "react-icons/md";
+
 
 export default function shopCartHeader() {
   return (
+
+    //? shopping cart component 
+
     <>
 
+    {/* curt header */}
 
-      <div className=" flex justify-between w-full">
-        <div>مشاهده سبد خرید</div>
-        <div>
-          <span>1</span>
+      <div className=" flex justify-between  w-full">
+
+        <a className="flex text-sm items-center cursor-pointer tracking-tighter text-orange-300"><MdOutlineArrowBackIos></MdOutlineArrowBackIos>مشاهده سبد خرید</a>
+
+        <div className=" text-gray-300 text-xs flex gap-1">
           <span>مورد</span>
+          <span>۱</span>
         </div>
+
       </div>
 
 
 
+    {/* product section */}
 
+      <div className=" flex justify-between h-32 my-3.5 shadow-light dark:shadow-dark rounded-2xl ">
 
+        <div className=" p-2.5 flex w-2/3 flex-col  h-full justify-between">
 
-
-
-
-
-      <div className=" flex justify-between h-32 shadow-light  dark:shadow-dark rounded-2xl ">
-
-
-
-        <div className=" flex w-2/3 flex-col  h-full justify-between">
-
-            <div className="flex justify-end text-right h-1/2 overflow-hidden">
-                قهوه اسپرسو مدل  پریکسا 
-                
-            </div>
+            <h1 className="flex text-base text-zinc-700 dark:text-white font-bold justify-end text-right h-1/2 line-clamp-2">قهوه اسپرسو مدل پرسیکا ۲۵۰ گرم </h1>
             
             <div className="flex h-1/2 flex-col">
-                <div className="flex justify-end gap-1.5"><span>تخفیف</span><span>۱۴،۵۰۰ </span></div>
-                <div className="flex justify-end gap-1.5"><span>تومان</span><span>۳۵۰</span></div>
+                <div className="flex  text-teal-600 justify-end gap-1.5 text-sm"><span>تخفیف</span><span>۱۴،۵۰۰ </span></div>
+                <div className="flex justify-end gap-1.5"><span>تومان</span><span className=" font-bold text-lg">۳۵۰,۰۰۰</span></div>
             </div>
 
         </div>
-
-
 
         <div className="flex w-1/3 items-center justify-center">
             <img className="" src="products/p1.png" alt="" />
         </div>
 
-
-
-      </div>
-      
+      </div>      
 
 
 
-
-
-
-
-
-
-
-
-
-      <div className="flex w-full justify-between border-t-2 pt-5">
-        <button className=" cursor-pointer rounded-2xl border-2 py-3.5 px-6">
+    {/* footer curt - price and buy-Btn */}
+      <div className="flex w-full justify-between border-t-2 border-white/10 pt-3">
+        <button className=" hover:bg-teal-500 bg-teal-600 cursor-pointer rounded-2xl py-3.5 px-6">
           ثبت سفارش
         </button>
-        <div className=" flex flex-col">
-          <span className=" text-xs">مبلغ قابل پرداخت</span>
-          <span className="flex">
-            <span>تومان</span>
-            <span>500,000</span>
+        <div className=" flex justify-center gap-1.5 text-right flex-col">
+          <span className=" text-[10px] font-medium border-gray-300">مبلغ قابل پرداخت</span>
+          <span className="flex items-center">
+            <span className="text-xs">تومان</span>
+            <span className=" font-bold text-xl">۵۰۰،۰۰۰</span>
           </span>
         </div>
       </div>
-
 
     </>
   );
