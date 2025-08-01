a201ee36dda33bfacd8d0ae169fe6cfcdd3b7978
Kaveh <kavehkhorshidiii@gmail.com>
2025-08-01 21:26:01 +0330

    update slider

--- src/components/Home/slider/slider.js
+++ src/components/Home/slider/slider.js
@@ -7,6 +7,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
+
 export default function slider() {
    return (
       <div className=" border-2">
@@ -20,13 +21,18 @@ export default function slider() {
             onSwiper={(swiper) => console.log(swiper)}
             onSlideChange={() => console.log("slide change")}
          >
-            <SwiperSlide className=" w-2xl h-16 border-2 border-amber-300">Slide 1</SwiperSlide>
-            <SwiperSlide className=" w-2xl h-16 border-2 border-amber-300">Slide 2</SwiperSlide>
+            <SwiperSlide><div className=" border-2 p-44 w-10 h-10">box 1</div></SwiperSlide>
+            <SwiperSlide><div className=" border-2 p-44 w-10 h-10">box 2</div></SwiperSlide>
+            <SwiperSlide><div className=" border-2 p-44 w-10 h-10">box 3</div></SwiperSlide>
+            <SwiperSlide><div className=" border-2 p-44 w-10 h-10">box 4</div></SwiperSlide>
+            <SwiperSlide><div className=" border-2 p-44 w-10 h-10">box 5</div></SwiperSlide>
+           
+            {/* <SwiperSlide className=" w-2xl h-16 border-2 border-amber-300">Slide 2</SwiperSlide>
             <SwiperSlide className=" w-2xl h-16 border-2 border-amber-300">Slide 3</SwiperSlide>
             <SwiperSlide className=" w-2xl h-16 border-2 border-amber-300">Slide 4</SwiperSlide>
             <SwiperSlide className=" w-2xl h-16 border-2 border-amber-300">Slide 5</SwiperSlide>
             <SwiperSlide className=" w-2xl h-16 border-2 border-amber-300">Slide 6</SwiperSlide>
-            <SwiperSlide className=" w-2xl h-16 border-2 border-amber-300">Slide 7</SwiperSlide>
+            <SwiperSlide className=" w-2xl h-16 border-2 border-amber-300">Slide 7</SwiperSlide> */}
          </Swiper>
       </div>
    )
