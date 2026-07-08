97fedb7aa88e70d43201feac00eadab20b6b2de6
Kaveh <kavehkhorshidiii@gmail.com>
2026-07-08 13:39:09 +0330

    commit

--- src/components/Home/aboutMe/aboutMe.js
+++ src/components/Home/aboutMe/aboutMe.js
@@ -8,10 +8,7 @@ export default function aboutMe() {
       <div className="pb-1">
          <Link href="https://github.com/Kaveh-Khorshidi">
             <div className="flex justify-center items-center gap-1">
-               <div>
-                  <Image width={50} height={50} src="/about me icons/github-svgrepo-com.svg" alt="social icons"></Image>
-               </div>
-               <div className=" font-bold text-3xl text-black dark:text-white">Kaveh khorshidi</div>
+               <div className=" font-bold text-sm text-black dark:text-white">Kaveh khorshidi</div>
             </div>
          </Link>
       </div>
